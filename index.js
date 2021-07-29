f76c84a16cd4eb0b34ccf88fde3e96a7b2583390
izilongwan <js.nb@qq.com>
2021-07-29 22:05:17 +0800

    feat：valueOf/toString应用

--- index.js
+++ index.js
@@ -510,3 +510,32 @@ const inherit = (() => {
     return Tar;
   }
 })();
+
+/**
+ * 实现a = ? 使a + a.a + a.a.a + ... == 'abc...'
+ * valueOf/toString应用
+ */
+var a = (function a(values) {
+  const key = arguments.callee.name;
+  const o = {
+    valueOf: myValueOf,
+    value: 0
+  };
+
+  function myValueOf () {
+    return values[o.value++];
+  }
+
+  let str = 'o';
+
+  for (let i = 1, len = values.length; i < len; i++) {
+    str += '[key]';
+    eval('(' + str + '= { valueOf:' + myValueOf + ' })');
+
+    // eval('(function () { return ' + str + ' = {}})()');
+    // obj.valueOf = myValueOf;
+  }
+
+  return o;
+
+})('abcdefghijklmn'.split(''))
