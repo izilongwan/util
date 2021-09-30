5b78a8e72779e8b878c36dfe398e87ef14475243
izilongwan <js.nb@qq.com>
2021-09-30 22:43:12 +0800

    fix: fix PageList

--- src/plugins/PageList.js
+++ src/plugins/PageList.js
@@ -1,5 +1,3 @@
-import { addEvent } from '../element/event'
-
 /**
  * 渲染页脚
  * @param {Element} wrap 元素 dom
@@ -7,147 +5,214 @@ import { addEvent } from '../element/event'
  * @param {Number} opt.pages 总页数
  * @param {Function} opt.callback 回调函数
  */
-export const PageList = (function (doc) {
-	var PageList = function (wrap, opt) {
-		this.wrap = doc.querySelector(wrap);
-		this.curPage = opt.curPage || 1;
-		this.pages = opt.pages || 0;
-	}
+export const PageList = ((doc) => {
+  var PageList = function (wrap, opt = {}) {
+    this.wrap = doc.querySelector(wrap);
+    this.curPage = opt.curPage || 1;
+    this.pages = opt.pages || parseInt(this.wrap.dataset.pages) || 0;
+    this.pages = Math.ceil(this.pages / (opt.pageSize || 1));
+    this.pageListClick = opt.pageListClick;
+    this.pageSearchBtnClick = opt.pageSearchBtnClick;
+  }
 
-	PageList.prototype = {
-		init: function () {
-			this.initPageList();
-			this.bindEvent();
-		},
+  PageList.prototype = {
+    init: function () {
+      if (!this.wrap) {
+        console.warn('pagination元素不存在');
+        return;
+      }
 
-		initPageList: function () {
-			var oDiv = doc.createElement('div');
-			oDiv.className = 'page-list J_pageList';
-			oDiv.innerHTML = this.renderPageList(this.curPage, this.pages);
-			this.wrap.appendChild(oDiv);
-			this.elem = doc.querySelector('.J_pageList');
-		},
+      this.initPageList();
+      this.bindEvent();
+    },
 
-		bindEvent: function () {
-			addEvent(this.elem, 'click', this.pageListClick.bind(this));
-		},
+    initPageList: function () {
+      var oList = doc.createElement('div'),
+          oSearch = doc.createElement('div'),
+          frag = doc.createDocumentFragment();
 
+      oList.className = 'page-list J_pageList';
+      oSearch.className = 'page-search-wrap';
+      oList.innerHTML = this.renderPageList(this.curPage, this.pages);
+      oSearch.innerHTML = this.pageTpl('search');
+      frag.appendChild(oList);
+      this.pages > 1 && frag.appendChild(oSearch);
+      this.wrap.appendChild(frag);
+      this.oList = oList;
+      this.oSearchInput = oSearch.querySelector('.J_page-search');
+      this.oSearchBtn = oSearch.querySelector('.J_page-search-btn');
+    },
 
-		pageListClick: function (e) {
-			var e = e || window.event,
-					tar = e.target || e.srcElement,
-					className = tar.className,
-					curPage = this.curPage,
-					pages = this.pages;
+    bindEvent: function () {
+      const { oList, oSearchBtn, oSearchInput } = this;
 
-			if (className) {
-				switch (className) {
-					case 'page-btn':
-						curPage = parseInt(tar.getAttribute('data-page'));
-						this.elem.innerHTML = this.renderPageList(curPage, pages);
-						typeof (callback) == 'function' && callback();
-						break;
-					case 'backward-btn':
-						curPage--;
-						this.elem.innerHTML = this.renderPageList(curPage, pages);
-						typeof (callback) == 'function' && callback();
-						break;
-					case 'forward-btn':
-						curPage++;
-						this.elem.innerHTML = this.renderPageList(curPage, pages);
-						typeof (callback) == 'function' && callback();
-						break;
-					default:
-						break;
-				}
-				this.curPage = curPage;
-				this.pages = pages;
-			}
-		},
+      oList.addEventListener('click', this.onPageListClick.bind(this));
+      oSearchInput && oSearchInput.addEventListener('input', this.onPageSearchInputChange.bind(this));
+      oSearchBtn && oSearchBtn.addEventListener('click', this.onPageSearchBtnClick.bind(this));
+    },
 
-		makeBtns: function (start, end, curPage) {
-			var list = '';
-			for (var i = start; i <= end; i++) {
-				list += this.pageBtnTpl('btn', i, curPage);
-			}
-			return list;
-		},
+    onPageSearchInputChange () {
+      const { oSearchInput } = this;
 
-		pageBtnTpl: function (type, num, cur, pages) {
-			switch (type) {
-				case 'btn':
-					return num === cur ?
-						'<span class="cur-page">' + num + '</span>' :
-						'<a class="page-btn" data-page=' + num + '>' + num + '</a>';
-					break;
-				case 'points':
-					return '<span class="points">…</span>';
-				case 'backward':
-					return cur === 1 ?
-						'<span class="disabled-btn">&lt;</span>' :
-						'<a class="backward-btn">&lt;</a>';
-				case 'forward':
-					return cur === pages ?
-						'<span class="disabled-btn">&gt;</span>' :
-						'<a class="forward-btn">&gt;</a>';
-					break;
-				default:
-					break;
-			}
-		},
+      oSearchInput.value = oSearchInput.value.replace(/\D|^0/g, '');
 
-		renderPageList: function (curPage, pages) {
-			if (pages <= 0) {
-				return '';
-			}
+      const { value } = oSearchInput;
 
-			if (pages == 1) {
-				return this.pageBtnTpl('btn', 1, 1, 1);
-			}
+      if (value) {
+        this.value = value.trim();
+      }
+    },
 
-			var btnGroup = this.pageBtnTpl('backward', '', curPage);
-			if (pages > 8) {
-				if (curPage < 3) {
-					btnGroup += this.makeBtns(1, 3, curPage) +
-						this.pageBtnTpl('points') +
-						this.makeBtns(pages - 1, pages, curPage);
-				} else if (curPage >= 3 && curPage < 5) {
-					btnGroup += this.makeBtns(1, curPage + 1, curPage) +
-						this.pageBtnTpl('points') +
-						this.makeBtns(pages - 1, pages, curPage);
-				} else if (curPage == 5) {
-					btnGroup += this.makeBtns(1, 2, curPage) +
-						this.pageBtnTpl('points') +
-						this.makeBtns(curPage - 1, curPage + 1, curPage) +
-						this.pageBtnTpl('points') +
-						this.makeBtns(pages - 1, pages, curPage);
-				} else if (curPage >= 6 && curPage < pages - 4) {
-					btnGroup += this.makeBtns(1, 2, curPage) +
-						this.pageBtnTpl('points') +
-						this.makeBtns(curPage - 2, curPage + 2, curPage) +
-						this.pageBtnTpl('points') +
-						this.makeBtns(pages - 1, pages, curPage);
-				} else if (curPage == pages - 4) {
-					btnGroup += this.makeBtns(1, 2, curPage) +
-						this.pageBtnTpl('points') +
-						this.makeBtns(curPage - 1, curPage + 1, curPage) +
-						this.pageBtnTpl('points') +
-						this.makeBtns(pages - 1, pages, curPage);
-				} else if (curPage >= pages - 3 && curPage <= pages - 2) {
-					btnGroup += this.makeBtns(1, 2, curPage) +
-						this.pageBtnTpl('points') +
-						this.makeBtns(curPage - 1, pages, curPage);
-				} else if (curPage > pages - 2 && curPage <= pages) {
-					btnGroup += this.makeBtns(1, 2, curPage) +
-						this.pageBtnTpl('points') +
-						this.makeBtns(pages - 2, pages, curPage);
-				}
-			} else {
-				btnGroup += this.makeBtns(1, pages, curPage);
-			}
-			return btnGroup += this.pageBtnTpl('forward', '', curPage, pages);
-		}
-	}
+    onPageSearchBtnClick () {
+      const { pageSearchBtnClick, value, pages } = this;
 
-	return PageList;
+      if (value && typeof pageSearchBtnClick === 'function') {
+        const val = parseInt(value);
+
+        if (val) {
+          if (val <= pages) {
+            pageSearchBtnClick(val);
+            this.oSearchInput.value = this.value = '';
+            return;
+          }
+
+          console.log('输入页码超过总页数');
+        }
+      }
+    },
+
+    onPageListClick: function (e) {
+      var e = e || window.event,
+          tar = e.target || e.srcElement,
+          className = tar.className,
+          curPage = this.curPage,
+          pages = this.pages;
+      const { pageListClick } = this;
+
+      if (className) {
+        switch (className) {
+          case 'page-btn':
+            curPage = parseInt(tar.getAttribute('data-page'));
+            this.oList.innerHTML = this.renderPageList(curPage, pages);
+            typeof (pageListClick) == 'function' && pageListClick(curPage, pages);
+            break;
+          case 'backward-btn':
+            curPage--;
+            this.oList.innerHTML = this.renderPageList(curPage, pages);
+            typeof (pageListClick) == 'function' && pageListClick(curPage, pages);
+            break;
+          case 'forward-btn':
+            curPage++;
+            this.oList.innerHTML = this.renderPageList(curPage, pages);
+            typeof (pageListClick) == 'function' && pageListClick(curPage, pages);
+            break;
+          default:
+            break;
+        }
+        this.curPage = curPage;
+        this.pages = pages;
+      }
+    },
+
+    makeBtns: function (start, end, curPage) {
+      var list = '';
+      for (var i = start; i <= end; i++) {
+        list += this.pageTpl('btn', i, curPage);
+      }
+      return list;
+    },
+
+    pageTpl: function (type, num, cur, pages) {
+      switch (type) {
+        case 'btn':
+          return num === cur ?
+            '<span class="cur-page">' + num + '</span>' :
+            '<a class="page-btn" data-page=' + num + '>' + num + '</a>';
+
+        case 'points':
+          return '<span class="points">……</span>';
+
+        case 'backward':
+          return cur === 1 ?
+            '<span class="disabled-btn">&lt;</span>' :
+            '<a class="backward-btn">&lt;</a>';
+
+        case 'forward':
+          return cur === pages ?
+            '<span class="disabled-btn">&gt;</span>' :
+            '<a class="forward-btn">&gt;</a>';
+
+        case 'search':
+          return ('<input type="text" placeholder="页码" class="page-search J_page-search" maxlength="5" />\
+                    <button class="page-search-btn J_page-search-btn">⇨</button>');
+        default:
+          break;
+      }
+    },
+
+    renderPageList: function (curPage, pages) {
+      if (pages <= 0) {
+        return '';
+      }
+
+      if (pages == 1) {
+        return this.pageTpl('btn', 1, 1, 1);
+      }
+
+      var btnGroup = this.pageTpl('backward', '', curPage);
+
+      if (pages > 8) {
+        if (curPage < 3) {
+          btnGroup += this.makeBtns(1, 3, curPage) +
+            this.pageTpl('points') +
+            this.makeBtns(pages - 1, pages, curPage);
+
+        } else if (curPage >= 3 && curPage < 5) {
+          btnGroup += this.makeBtns(1, curPage + 1, curPage) +
+            this.pageTpl('points') +
+            this.makeBtns(pages - 1, pages, curPage);
+
+        } else if (curPage == 5) {
+          btnGroup += this.makeBtns(1, 2, curPage) +
+            this.pageTpl('points') +
+            this.makeBtns(curPage - 1, curPage + 1, curPage) +
+            this.pageTpl('points') +
+            this.makeBtns(pages - 1, pages, curPage);
+
+        } else if (curPage >= 6 && curPage < pages - 4) {
+          btnGroup += this.makeBtns(1, 2, curPage) +
+            this.pageTpl('points') +
+            this.makeBtns(curPage - 2, curPage + 2, curPage) +
+            this.pageTpl('points') +
+            this.makeBtns(pages - 1, pages, curPage);
+
+        } else if (curPage == pages - 4) {
+          btnGroup += this.makeBtns(1, 2, curPage) +
+            this.pageTpl('points') +
+            this.makeBtns(curPage - 1, curPage + 1, curPage) +
+            this.pageTpl('points') +
+            this.makeBtns(pages - 1, pages, curPage);
+
+        } else if (curPage >= pages - 3 && curPage <= pages - 2) {
+          btnGroup += this.makeBtns(1, 2, curPage) +
+            this.pageTpl('points') +
+            this.makeBtns(curPage - 1, pages, curPage);
+
+        } else if (curPage > pages - 2 && curPage <= pages) {
+          btnGroup += this.makeBtns(1, 2, curPage) +
+            this.pageTpl('points') +
+            this.makeBtns(pages - 2, pages, curPage);
+        }
+
+      } else {
+        btnGroup += this.makeBtns(1, pages, curPage);
+      }
+
+      btnGroup += this.pageTpl('forward', '', curPage, pages);
+
+      return btnGroup;
+    }
+  }
+
+  return PageList;
 })(globalThis.document);
